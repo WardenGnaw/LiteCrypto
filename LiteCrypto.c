ed2d660acc641b7237566df66a153bcc13862b90
Andrew Wang <awang24@calpoly.edu>
2014-11-23 18:02:58 -0800

    Added Sign and Verify, partly implemented

--- LiteCrypto.c
+++ LiteCrypto.c
@@ -1,17 +1,62 @@
 #include "LiteCrypto.h"
 
-u64 packet_sign(u8 pub_key, u8 **data, u64 size) {
+u64 packet_sign(u8 *secret_key, u8 **message, u64 message_length) {
+   int fd = open("/dev/urandom", O_RDONLY);
+   u8 random_key[crypto_onetimeauth_KEYBYTES];
+   u8 out[crypto_onetimeauth_key_and_bytes];
+   u8 *ret = malloc(message_length + crypto_onetimeauth_key_and_bytes); // Allocate space for new signed packet
 
-   return 0;
+   if (NULL == ret) {
+      perror("Failed to allocate space for new signed packet.\n");
+      return -1; // Error failed to allocate space for signed packet.
+   }
+
+   if (fd < 0) {
+      perror("Failed to open urandom.\n");
+      return -2; // Error failed to open urandom.
+   }
+   
+   if (read(fd, random_key, crypto_onetimeauth_KEYBYTES) == -1) {
+      perror("Failed to read urandom.\n");
+      return -2; // Error failed to open urandom.
+   }
+
+   crypto_onetimeauth(out + crypto_onetimeauth_KEYBYTES, *message, message_length, random_key); 
+
+   crypto_secret_box(out, random_key, &message_length /* INSERT NONCE_HERE */, secret_key);
+
+   memcpy(ret, *message, IPV4_AND_UDP_HEADER_SIZE);
+   memcpy(ret + IPV4_AND_UDP_HEADER_SIZE, out, crypto_onetimeauth_KEYBYTES);
+   memcpy(ret + IPV4_AND_UDP_HEADER_SIZE + crypto_onetimeauth_KEYBYTES, out + crypto_onetimeauth_KEYBYTES, crypto_onetimeauth_BYTES);
+   memcpy(ret + IPV4_AND_UDP_HEADER_SIZE + crypto_onetimeauth_key_and_bytes, 
+          *message + IPV4_AND_UDP_HEADER_SIZE, 
+          message_length - IPV4_AND_UDP_HEADER_SIZE);  
+
+   free(*message);                                           // Get rid of old data
+   *message = ret;                                           // Return encrypted data
+   memset(random_key, 0, crypto_onetimeauth_KEYBYTES);       // Clear key
+   close(fd);                                                // Close urandom
+   return message_length + crypto_onetimeauth_key_and_bytes; // Return new size
 }
 
-u64 packet_verify(u8 **data, u64 size) {
+u64 packet_verify(u8 *key, u8 **message, u64 message_length) {
+   u8 authorization[crypto_onetimeauth_BYTES]; 
+   u8 *ret = malloc(message_length - crypto_onetimeauth_key_and_bytes);
+   
+   memcpy(ret, *message, IPV4_AND_UDP_HEADER_SIZE);
+   memcpy(authorization, *message + IPV4_AND_UDP_HEADER_SIZE, crypto_onetimeauth_BYTES); 
+   memcpy(ret + IPV4_AND_UDP_HEADER_SIZE, 
+          *message + IPV4_AND_UDP_HEADER_SIZE + crypto_onetimeauth_key_and_bytes, 
+          message_length - IPV4_AND_UDP_HEADER_SIZE);  
 
+   
+   crypto_onetimeauth_verify(authorization, ret, message_length - crypto_onetimeauth_key_and_bytes, key);
+   
    return 0;
 }
 
 u64 packet_secret_box(u8 **data, u64 size) {
-
+   
    return 0;
 }
 
--- LiteCrypto.h
+++ LiteCrypto.h
@@ -4,25 +4,47 @@
 #ifndef LITECRYPTO_H
 #define LITECRYPTO_H
 #include "tweetnacl.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stdint.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define IPV4_HEADER_SIZE 20
+#define UDP_HEADER_SIZE 4
+#define IPV4_AND_UDP_HEADER_SIZE 24
+#define crypto_onetimeauth_key_and_bytes (crypto_onetimeauth_KEYBYTES + crypto_onetimeauth_BYTES)
+
+typedef unsigned char u8;
+typedef unsigned long u32;
+typedef unsigned long long u64;
+typedef long long i64;
+/*
+typedef struct  __attribute__ ((__packed__)) {
+   u8 ipv4_header[IPV4_HEADER_SIZE];
+   u8 udp_header[UDP_HEADER_SIZE];
+   u8 key[crypto_onetimeauth_KEYBYTES]; // defined in tweetnacl.h as 32
+   u8 auth[crypto_onetimeauth_BYTES];   // defined in tweetnacl.h as 16
+}  __attribute__ ((__packed__)) packet_header_struct;
+*/
+/*
+ * 
+ */
+u64 packet_sign(u8 *key, u8 **data, u64 size);
 
 /*
  * 
  */
-uint32_t packet_sign(uint8_t **data, uint32_t size);
+u64 packet_verify(u8 *key, u8 **message, u64 message_length);
 
 /*
  * 
  */
-uint32_t packet_verify(uint8_t **data, uint32_t size);
+u64 packet_secret_box(u8 **data, u64 size);
 
 /*
  * 
  */
-uint32_t packet_secret_box(uint8_t **data, uint32_t size);
-
-/*
- * 
- */
-uint32_t packet_open_secret_box(uint8_t **data, uint32_t size);
+u64 packet_open_secret_box(u8 **data, u64 size);
 #endif
