aa604cf2165aebb807d5f008826455c7181f1a96
Sam Wu <sjiwu14@gmail.com>
2014-12-02 20:22:07 -0800

    Adding extern to functions prototyped in LiteCrypto.h

--- LiteCrypto.h
+++ LiteCrypto.h
@@ -38,7 +38,7 @@ typedef long long i64;
  *  output_key to have enough space for KEY_SIZE
  *  salt to be of length SALT_SIZE
  */
-i64 derive_key(u8 *output_key, u8 *input_key, i64 input_size, u8 *salt);
+extern i64 derive_key(u8 *output_key, u8 *input_key, i64 input_size, u8 *salt);
 
 /*
  * Authenticate IPv4 + UDP packet by HMAC-SHA512-256.
@@ -48,7 +48,7 @@ i64 derive_key(u8 *output_key, u8 *input_key, i64 input_size, u8 *salt);
  *
  * Returns: size of signed_data
  */
-i64 packet_sign(u8 *signed_data, u8 *key, u8 *data, i64 size);
+extern i64 packet_sign(u8 *signed_data, u8 *key, u8 *data, i64 size);
 
 /*
  * Verify authentication of a signed IPv4 + UDP packet by HMAC-SHA512-256.
@@ -59,7 +59,7 @@ i64 packet_sign(u8 *signed_data, u8 *key, u8 *data, i64 size);
  * Returns:
  *  size of output if verified, -1 if not.
  */
-i64 packet_verify(u8 *output, u8 *key, u8 *signed_data, i64 signed_size);
+extern i64 packet_verify(u8 *output, u8 *key, u8 *signed_data, i64 signed_size);
 
 /*
  * Encrypts data of a IPv4 + UDP packet by xsalsa20 stream cipher.
@@ -69,7 +69,7 @@ i64 packet_verify(u8 *output, u8 *key, u8 *signed_data, i64 signed_size);
  *
  * Returns: size of output
  */
-i64 packet_encrypt(u8 *output, u8 *key, u8 *data, i64 size);
+extern i64 packet_encrypt(u8 *output, u8 *key, u8 *data, i64 size);
 
 /*
  * Decrypts data of an encrypted IPv4 + UDP packet by xsalsa20 stream cipher.
@@ -79,7 +79,7 @@ i64 packet_encrypt(u8 *output, u8 *key, u8 *data, i64 size);
  *
  * Returns: size of output
  */
-i64 packet_decrypt(u8 *output, u8 *key, u8 *data, i64 size);
+extern i64 packet_decrypt(u8 *output, u8 *key, u8 *data, i64 size);
 
 /*
  * Encrypts data of a IPv4 + UDP packet by xsalsa20 stream cipher and
@@ -91,7 +91,7 @@ i64 packet_decrypt(u8 *output, u8 *key, u8 *data, i64 size);
  *
  * Returns: size of signed_data on success or -1 on failure
  */
-i64 packet_encrypt_sign(u8 *signed_data, u8 *key, u8 *data, i64 size);
+extern i64 packet_encrypt_sign(u8 *signed_data, u8 *key, u8 *data, i64 size);
 
 /*
  * Verify IPv4 + UDP packet by HMAC-SHA512-256, if verified then
@@ -103,6 +103,6 @@ i64 packet_encrypt_sign(u8 *signed_data, u8 *key, u8 *data, i64 size);
  *
  * Returns: size of output on success or -1 on failure
  */
-i64 packet_verify_decrypt(u8 *output, u8 *key, u8 *signed_data, i64 signed_size);
+extern i64 packet_verify_decrypt(u8 *output, u8 *key, u8 *signed_data, i64 signed_size);
 
 #endif
